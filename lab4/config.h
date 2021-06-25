3b12c7915f79b839a8dae9bde68ff3775808604f
Jeff Goeders <jeff.goeders@gmail.com>
2021-06-25 09:30:23 -0600

    Fix config defines
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- lab4/config.h
+++ lab4/config.h
@@ -1,6 +1,6 @@
 #ifndef LAB4_CONFIG
 #define LAB4_CONFIG
 
-#define TIMER_PERIOD 50.0E-3
+#define CONFIG_TIMER_PERIOD 50.0E-3
 
 #endif /* LAB4_CONFIG */
--- lab6/main.c
+++ lab6/main.c
@@ -49,44 +49,11 @@ For questions, contact Brad Hutchings or Jeff Goeders, https://ece.byu.edu/
 // Compute the timer clock freq.
 #define TIMER_CLOCK_FREQUENCY (XPAR_CPU_CORTEXA9_0_CPU_CLK_FREQ_HZ / 2)
 // Compute timer load value.
-#define TIMER_LOAD_VALUE ((TIMER_PERIOD * TIMER_CLOCK_FREQUENCY) - 1.0)
-#define FOREVER 1 // syntactic sugar for while-forever loop.
+#define TIMER_LOAD_VALUE ((CONFIG_TIMER_PERIOD * TIMER_CLOCK_FREQUENCY) - 1.0)
 
-// Code below is associated with the tick timer that is not
-// currently used in this main().
-
-// The max duration across all ticks.
-static double maxDuration_g;
-// Name of the function with longest duration is here.
-static const char *maxDurationFunctionName_g;
-
-#define MAX_DURATION_TIMER_INDEX 2
-
-// Use this function to time each tick.
-typedef void(functionPointer_t)();
-#define SIMON_CONTROL_TICK "simonControl_tick()"
-#define VERIFY_SEQUENCE_TICK "verifySequence_tick()"
-#define FLASH_SEQUENCE_TICK "flashSequency_tick()"
-#define BUTTON_HANDLER_TICK "buttonHandler_tick()"
-#define WIN_SCREEN_TICK "winScreen_tick()"
-
-// This assumes that the interval timer was initialized.
-static void tickTimer(functionPointer_t *fp, const char *functionName) {
-  intervalTimer_reset(MAX_DURATION_TIMER_INDEX); // Reset the timer.
-  intervalTimer_start(MAX_DURATION_TIMER_INDEX); // Start the timer.
-  fp(); // Invoke the tick function passed in by argument.
-  intervalTimer_stop(MAX_DURATION_TIMER_INDEX); // Stop the timer.
-  double duration;                              // Keep track of tick duration.
-  duration = intervalTimer_getTotalDurationInSeconds(
-      MAX_DURATION_TIMER_INDEX);  // Get the duration.
-  if (maxDuration_g < duration) { // Keep the max duration.
-    maxDuration_g = duration;
-    maxDurationFunctionName_g =
-        functionName; // Keep track of which function had the max. duration.
-  }
-}
-
-/**************** main() code starts here. ************************/
+#define INTERRUPTS_PER_SECOND (1.0 / CONFIG_TIMER_PERIOD)
+#define TOTAL_SECONDS 45
+#define MAX_INTERRUPT_COUNT (INTERRUPTS_PER_SECOND * TOTAL_SECONDS)
 
 /****************************** RUN_BUTTON_HANDLER_TEST ****************/
 #if RUN_PROGRAM == MILESTONE_1
@@ -96,7 +63,7 @@ static void test_init() {
   printf("Running the buttonHandler test.\n");
 }
 
-void isr_function() {
+void tickAll() {
   buttonHandler_tick();
   bhTester_tick();
 }
@@ -110,7 +77,7 @@ static void test_init() {
   printf("Running the flashSequence test.\n");
 }
 
-void isr_function() {
+void tickAll() {
   fsTester_tick();
   flashSequence_tick();
 }
@@ -122,7 +89,7 @@ static void test_init() {
   printf("Running the verifySequence test.\n");
 }
 
-void isr_function() {
+void tickAll() {
   buttonHandler_tick();
   flashSequence_tick();
   verifySequence_tick();
@@ -143,7 +110,7 @@ static void test_init() {
   printf("Running the simon game.\n");
 }
 
-void isr_function() {
+void tickAll() {
   simonControl_tick();
   buttonHandler_tick();
   flashSequence_tick();
@@ -156,11 +123,10 @@ void isr_function() {
 int main() {
   test_init(); // Program specific.
   // Init all interrupts (but does not enable the interrupts at the devices).
-  // Prints an error message if an internal failure occurs if the argument
+  // Prints an error message if an internal failure occurs because the argument
   // = true.
-  interrupts_initAll(1);
+  interrupts_initAll(true);
   interrupts_setPrivateTimerLoadValue(TIMER_LOAD_VALUE);
-  u32 privateTimerTicksPerSecond = interrupts_getPrivateTimerTicksPerSecond();
   interrupts_enableTimerGlobalInts();
   // Keep track of your personal interrupt count. Want to make sure that you
   // don't miss any interrupts.
@@ -169,9 +135,24 @@ int main() {
   interrupts_startArmPrivateTimer();
   // Enable interrupts at the ARM.
   interrupts_enableArmInts();
-  // main() must NOT terminate or prints to the terminal will not work
-  // on the board (emulator is OK).
-  while (FOREVER) {
-    utils_sleep();
+  while (1) {
+    if (interrupts_isrFlagGlobal) {
+      // Count ticks.
+      personalInterruptCount++;
+      tickAll();
+      interrupts_isrFlagGlobal = 0;
+      if (personalInterruptCount >= MAX_INTERRUPT_COUNT)
+        break;
+      utils_sleep();
+    }
   }
+  interrupts_disableArmInts();
+  printf("isr invocation count: %d\n", interrupts_isrInvocationCount());
+  printf("internal interrupt count: %d\n", personalInterruptCount);
+  return 0;
+}
+
+// Interrupt routine
+void isr_function() {
+  // Empty for flag method (flag set elsewhere)
 }
