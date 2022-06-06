45ce0cd6610d323dc353282f077f8a81b295e140
Jeff Goeders <jeff.goeders@gmail.com>
2022-06-06 12:55:07 -0600

    Update lab5 main
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- lab5_touchscreen/main.c
+++ lab5_touchscreen/main.c
@@ -6,7 +6,9 @@
 #include "touchscreen.h"
 #include "utils.h"
 
-#define DELAY_MS 10
+#define PERIOD_MS 10
+#define CIRCLE_RADIUS 40
+#define MS_PER_S 1000
 
 volatile bool press_detected = false;
 volatile display_point_t point;
@@ -14,7 +16,7 @@ volatile display_point_t point;
 void isr() {
   // Acknowledge interrupt
   intervalTimer_ackInterrupt(INTERVAL_TIMER_TIMER_0);
-  interrupts_ack(INTERRUPTS_IRQ_TIMER_0);
+  interrupts_ack(1 << INTERRUPTS_IRQ_TIMER_0);
 
   // Repeatedly tick the touch screen state machine
   touchscreen_tick();
@@ -22,39 +24,44 @@ void isr() {
   // Get the current status of the touchscreen
   enum touchscreen_status_e status = touchscreen_get_status();
 
-  if (!press_detected && status != TOUCHSCREEN_IDLE) {
+  if (!press_detected && (status != TOUCHSCREEN_IDLE)) {
     // New button press detected, draw filled circle
     point = touchscreen_get_location();
     press_detected = true;
-    display_fillCircle(point.x, point.y, 40, DISPLAY_RED);
+    printf("x: %hu y: %hu\n", point.x, point.y);
+    display_fillCircle(point.x, point.y, CIRCLE_RADIUS, DISPLAY_RED);
   }
 
-  if (press_detected && status == TOUCHSCREEN_RELEASED) {
+  if (press_detected && (status == TOUCHSCREEN_RELEASED)) {
     // Press released, draw empty circle and acknowledge press
     press_detected = false;
-    display_fillCircle(point.x, point.y, 40, DISPLAY_BLACK);
-    display_drawCircle(point.x, point.y, 40, DISPLAY_RED);
+    display_fillCircle(point.x, point.y, CIRCLE_RADIUS, DISPLAY_BLACK);
+    display_drawCircle(point.x, point.y, CIRCLE_RADIUS, DISPLAY_RED);
     touchscreen_ack_touch();
   }
 }
 
 int main() {
-  printf("========== Starting touchscreen test program ==========");
+  printf("========== Starting touchscreen test program ==========\n");
 
   // Initialize drivers
   display_init();
   interrupts_init();
-  touchscreen_init(DELAY_MS);
+  touchscreen_init(PERIOD_MS);
 
   // Fill screen black
   display_fillScreen(DISPLAY_BLACK);
 
   // Set up interrupts
   interrupts_register(INTERRUPTS_IRQ_TIMER_0, isr);
-  intervalTimer_initCountDown(INTERVAL_TIMER_TIMER_0, 0.01);
+
+  intervalTimer_initCountDown(INTERVAL_TIMER_TIMER_0,
+                              PERIOD_MS / (double)MS_PER_S);
+  intervalTimer_enableInterrupt(INTERVAL_TIMER_TIMER_0);
+  intervalTimer_start(INTERVAL_TIMER_TIMER_0);
+  interrupts_irq_enable(1 << INTERRUPTS_IRQ_TIMER_0);
 
   while (1) {
-    utils_msDelay(1);
   }
   return 0;
 }
