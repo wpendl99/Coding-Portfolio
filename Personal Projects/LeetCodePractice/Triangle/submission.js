58dd20d65b9c0344e55eafb23eb712cfaea27427
wpendl99 <57572263+wpendl99@users.noreply.github.com>
2022-06-14 00:40:10 -0600

    Fixed Mismatched Brackets in Solution

--- Personal Projects/LeetCodePractice/Triangle/submission.js
+++ Personal Projects/LeetCodePractice/Triangle/submission.js
@@ -4,19 +4,20 @@
  * @param {number[][]} triangle
  * @return {number}
  */
-var minimumTotal = function(triangle) {
-    let arr = triangle[triangle.length-1]; // Set the initial array to the last row of triangle
-    
-    // Loop through every row
-    for(let i = triangle.length - 2; i >= 0; i--){
-        let row = arr
-        // Loop through every index (j) in current row (i) and see whether (j) or (j+1) of the following row (i+1) are lower
-        for(let j = 0; j < i + 1; j++){
-            if(row[j] < row[j+1]){
-                arr[j] = row[j] + triangle[i][j]
-            } else {
-                arr[j] = row[j+1] + triangle[i][j]
-        }
-    }
-    return arr[0]
+var minimumTotal = function (triangle) {
+	let arr = triangle[triangle.length - 1]; // Set the initial array to the last row of triangle
+
+	// Loop through every row
+	for (let i = triangle.length - 2; i >= 0; i--) {
+		let row = arr;
+		// Loop through every index (j) in current row (i) and see whether (j) or (j+1) of the following row (i+1) are lower
+		for (let j = 0; j < i + 1; j++) {
+			if (row[j] < row[j + 1]) {
+				arr[j] = row[j] + triangle[i][j];
+			} else {
+				arr[j] = row[j + 1] + triangle[i][j];
+			}
+		}
+	}
+	return arr[0];
 };
