677a6fb3f5ab84b2c70e426e2cc17ba6ed92ac08
Jeff Goeders <jeff.goeders@gmail.com>
2022-07-01 09:01:06 -0600

    lab 5 updates
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- lab5_touchscreen/main.c
+++ lab5_touchscreen/main.c
@@ -7,38 +7,72 @@
 #include "utils.h"
 
 #define PERIOD_S 0.01
-#define CIRCLE_RADIUS 40
+#define CIRCLE_RADIUS_MAX 50
 #define MS_PER_S 1000
 
 volatile bool press_detected = false;
 volatile display_point_t point;
 
+static enum {
+  TEST_IDLE_ST,
+  TEST_TOUCH_PRESSED_ST,
+  TEST_TOUCH_RELEASED_ST
+} currentState = TEST_IDLE_ST;
+
+uint8_t radius;
+
+void test_tick() {
+  // Get the current status of the touchscreen
+  touchscreen_status_t status = touchscreen_get_status();
+
+  // Transition
+  switch (currentState) {
+  case TEST_IDLE_ST:
+    if (status != TEST_IDLE_ST) {
+      currentState = TEST_TOUCH_PRESSED_ST;
+
+      // New button press detected, draw filled circle
+      point = touchscreen_get_location();
+      radius = 1;
+      printf("x: %hu y: %hu\n", point.x, point.y);
+    }
+    break;
+  case TEST_TOUCH_PRESSED_ST:
+    if (status == TOUCHSCREEN_RELEASED)
+      currentState = TEST_TOUCH_RELEASED_ST;
+    break;
+  case TEST_TOUCH_RELEASED_ST:
+    currentState = TEST_IDLE_ST;
+    break;
+  }
+
+  // Action
+  switch (currentState) {
+  case TEST_TOUCH_PRESSED_ST:
+    if (radius < CIRCLE_RADIUS_MAX) {
+      display_fillCircle(point.x, point.y, radius, DISPLAY_RED);
+      radius++;
+    }
+    break;
+  case TEST_TOUCH_RELEASED_ST:
+    // Press released, draw empty circle and acknowledge press
+    press_detected = false;
+    display_fillCircle(point.x, point.y, radius, DISPLAY_BLACK);
+    display_drawCircle(point.x, point.y, radius, DISPLAY_RED);
+    touchscreen_ack_touch();
+    break;
+  }
+}
+
 void isr() {
+
   // Acknowledge interrupt
   intervalTimer_ackInterrupt(INTERVAL_TIMER_TIMER_0);
   interrupts_ack(1 << INTERRUPTS_TIMER_0_IRQ);
 
   // Repeatedly tick the touch screen state machine
   touchscreen_tick();
-
-  // Get the current status of the touchscreen
-  touchscreen_status_t status = touchscreen_get_status();
-
-  if (!press_detected && (status != TOUCHSCREEN_IDLE)) {
-    // New button press detected, draw filled circle
-    point = touchscreen_get_location();
-    press_detected = true;
-    printf("x: %hu y: %hu\n", point.x, point.y);
-    display_fillCircle(point.x, point.y, CIRCLE_RADIUS, DISPLAY_RED);
-  }
-
-  if (press_detected && (status == TOUCHSCREEN_RELEASED)) {
-    // Press released, draw empty circle and acknowledge press
-    press_detected = false;
-    display_fillCircle(point.x, point.y, CIRCLE_RADIUS, DISPLAY_BLACK);
-    display_drawCircle(point.x, point.y, CIRCLE_RADIUS, DISPLAY_RED);
-    touchscreen_ack_touch();
-  }
+  test_tick();
 }
 
 int main() {
