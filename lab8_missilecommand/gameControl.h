74fb34b2eedcc1050257c3eaa7d6b5777ed6c2dc
Jeff Goeders <jeff.goeders@gmail.com>
2022-07-20 23:55:29 -0600

    Lab 8
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- lab8_missilecommand/gameControl.h
+++ lab8_missilecommand/gameControl.h
@@ -1,8 +1,18 @@
 #ifndef GAMECONTROL
 #define GAMECONTROL
 
-void gameControl_init();
+#include <stdbool.h>
 
+// Initialize the game control logic
+// milestone3 - whether to include milestone 3 features (plane and stats)
+//
+// This function will initialize all missiles, stats, plane, etc.
+void gameControl_init(bool milestone3);
+
+// Tick the game control logic
+//
+// This function should tick the missiles, handle screen touches, collisions,
+// and updating statistics.
 void gameControl_tick();
 
 #endif /* GAMECONTROL */
