34566d5965064c08e9bcf2c653f32080dabce5a2
Jeff Goeders <jeff.goeders@gmail.com>
2022-08-17 14:26:50 -0600

    Fix touchscreen interrupts for lab8
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- lab8_missilecommand/main_m2.c
+++ lab8_missilecommand/main_m2.c
@@ -9,36 +9,51 @@
 #include "touchscreen.h"
 
 #define RUNTIME_S 30
-#define RUNTIME_TICKS ((int)(RUNTIME_S / CONFIG_TIMER_PERIOD))
+#define RUNTIME_TICKS ((int)(RUNTIME_S / CONFIG_GAME_TIMER_PERIOD))
 
 volatile bool interrupt_flag;
 
 uint32_t isr_triggered_count;
 uint32_t isr_handled_count;
 
-void isr() {
+// Interrupt handler for game - use flag method so that it can be interrupted by
+// the touchscreen tick while running.
+void game_isr() {
   intervalTimer_ackInterrupt(INTERVAL_TIMER_0);
   interrupt_flag = true;
   isr_triggered_count++;
 }
 
+// Interrupt handler for touchscreen - tick directly
+void touchscreen_isr() {
+  intervalTimer_ackInterrupt(INTERVAL_TIMER_1);
+  touchscreen_tick();
+}
+
+// Milestone 2 test application
 int main() {
   interrupt_flag = false;
   isr_triggered_count = 0;
   isr_handled_count = 0;
 
   display_init();
-  touchscreen_init(CONFIG_TIMER_PERIOD);
-  gameControl_init(false);
+  touchscreen_init(CONFIG_TOUCHSCREEN_TIMER_PERIOD);
+  gameControl_init();
 
   // Initialize timer interrupts
   interrupts_init();
-  interrupts_register(INTERVAL_TIMER_0_INTERRUPT_IRQ, isr);
+  interrupts_register(INTERVAL_TIMER_0_INTERRUPT_IRQ, game_isr);
+  interrupts_register(INTERVAL_TIMER_1_INTERRUPT_IRQ, touchscreen_isr);
   interrupts_irq_enable(INTERVAL_TIMER_0_INTERRUPT_IRQ);
+  interrupts_irq_enable(INTERVAL_TIMER_1_INTERRUPT_IRQ);
 
-  intervalTimer_initCountDown(INTERVAL_TIMER_0, CONFIG_TIMER_PERIOD);
+  intervalTimer_initCountDown(INTERVAL_TIMER_0, CONFIG_GAME_TIMER_PERIOD);
+  intervalTimer_initCountDown(INTERVAL_TIMER_1,
+                              CONFIG_TOUCHSCREEN_TIMER_PERIOD);
   intervalTimer_enableInterrupt(INTERVAL_TIMER_0);
+  intervalTimer_enableInterrupt(INTERVAL_TIMER_1);
   intervalTimer_start(INTERVAL_TIMER_0);
+  intervalTimer_start(INTERVAL_TIMER_1);
 
   // Main game loop
   while (isr_triggered_count < RUNTIME_TICKS) {
@@ -48,7 +63,6 @@ int main() {
     isr_handled_count++;
 
     gameControl_tick();
-    touchscreen_tick();
   }
   printf("Handled %d of %d interrupts\n", isr_handled_count,
          isr_triggered_count);
