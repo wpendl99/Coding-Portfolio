6278fc8aa0112f13b7fa03e65e69e93d24b29e49
Jeff Goeders <jeff.goeders@gmail.com>
2022-08-17 11:24:22 -0600

    fix comments
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- lab4_interrupts/main.c
+++ lab4_interrupts/main.c
@@ -5,8 +5,8 @@
 #include "interrupt_test.h"
 #include "interrupts.h"
 
+// Run the interrupt test to test the driver for the AXI Interrupt Controller
 int main() {
-  // Run the interrupt test to test the driver for the AXI Interrupt Controller
   printf("Running the interrupt test\n");
   interrupt_test_run();
 }
